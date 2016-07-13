97fa7d6c97899a587bcb1315bfb5553115c32437
Chanwoo Park <parkchan@brandeis.edu>
2016-07-13 14:01:43 -0400

    login works

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -1,34 +1,3 @@
-if(Meteor.isClient) {
-Template.register.events({
-  'submit .form-register': function(event){
-    event.preventDefault();
-    var emailVar = $('[name=js-email]').val();
-    var passwordVar = $('[name=js-password]').val();
-    console.dir(emailVar);
-    console.dir(passwordVar);
-
-
-    Accounts.createUser({
-      email: emailVar,
-      password: passwordVar
-    }, function(error){
-      if(error){
-        console.log(error.reason);
-        return;
-      } else {
-        console.log("created Account")
-        Router.go('home');
-      }
-
-    });
-
-    console.log("accounts createuser...");
-
-
-  }
-
-});
-
 Template.login.events({
   'submit .form-login': function(event){
     event.preventDefault();
@@ -39,4 +8,3 @@ Template.login.events({
     Router.go('home');
   }
 });
-}
---
+++ pages/signup/client/signup.js
@@ -0,0 +1,31 @@
+Template.register.events({
+  'submit .form-register': function(event){
+    event.preventDefault();
+    var emailVar = $('[name=js-email]').val();
+    var passwordVar = $('[name=js-password]').val();
+    var typeVar = $('[name=usertype]').val();
+    console.dir(emailVar);
+    console.dir(passwordVar);
+    console.dir(typeVar);
+
+
+    Accounts.createUser({
+      email: emailVar,
+      password: passwordVar
+    }, function(error){
+      if(error){
+        console.log(error.reason);
+        return;
+      } else {
+        console.log("created Account")
+        Router.go('home');
+      }
+
+    });
+
+    console.log("accounts createuser...");
+
+
+  }
+
+});
