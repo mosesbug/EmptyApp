46b77ecc37e48c6a441264f843ffcd24f5c73380
gclevin <Gabe@Gabriels-MacBook.local>
2016-07-20 11:51:47 -0400

    Dialog Box Change

--- pages/courses/client/courses.js
+++ pages/courses/client/courses.js
@@ -18,15 +18,15 @@ Template.showcourse.events({
 		event.preventDefault();
 		console.log(this._id);
 
-			var numClasses = Meteor.user().profile.classes.length
-			for(i = 0; i < numClasses; i++) {
-				if(Meteor.user().profile.classes[i]===this._id) {
-					console.log("you've already added the class")
-					throw new UserException("Invalid");
-
-				}
+		var numClasses = Meteor.user().profile.classes.length
+		for(i = 0; i < numClasses; i++) {
+			if(Meteor.user().profile.classes[i]===this._id) {
+				console.log("you've already added the class")
+				throw new UserException("Invalid");
 
 			}
+
+		}
 
 
 		Meteor.users.update(Meteor.userId(), {$push: {"profile.classes": this._id }});
@@ -37,7 +37,7 @@ Template.showcourse.events({
 
 
 
-	 	Router.go("/coursePage/"+this._id);
+		Router.go("/coursePage/"+this._id);
 
 
 	},
@@ -46,12 +46,33 @@ Template.showcourse.events({
 		event.preventDefault();
 		console.log("Clicked the remove button");
 
-		if(Meteor.user() && Meteor.userId() === this.instructor._id ) {
-			Courses.remove({_id: this._id});
-			Router.go("/courses");
-		} else {
-			console.log("you don't have access to delete this class");
-		}
+		var r = confirm("Are you sure you would like to delete this class?");
+		if (r == true) {
+			if(Meteor.user() && Meteor.userId() === this.instructor._id ) {
+				Courses.remove({_id: this._id});
+				Router.go("/courses");
+			} else {
+				window.alert("You don't have access to delete this class");
+				console.log("you don't have access to delete this class");
+			}
+		} 
+
+
+		// if(Meteor.user() && Meteor.userId() === this.instructor._id ) {
+
+		// 	var r = confirm("Are you sure you would like to delete this class?");
+		// 	if (r == true) {
+		// 		Courses.remove({_id: this._id});
+		// 		Router.go("/courses");
+		// 		x = "Class deleted.";
+		// 	} else {
+		// 		x = "Class NOT removed.";
+		// 	}
+
+		// } else {
+		// 	window.alert("you don't have access to delete this class");
+		// 	console.log("you don't have access to delete this class");
+		// }
 
 
 
