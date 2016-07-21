4bdffec63687cff719880c4a9517e85462707dd6
gclevin <Gabe@Gabriels-MacBook.local>
2016-07-21 11:25:51 -0400

    Dropdown Selectors

--- server/startup.js
+++ server/startup.js
@@ -1,5 +1,15 @@
 Meteor.startup(function(){
 	//Recordings.remove({});
+	Languages.insert({name: "English"});
+	Languages.insert({name: "German"});
+	Languages.insert({name: "Chinese"});
+	Languages.insert({name: "Spanish"});
+	Languages.insert({name: "French"});
+
+	Schools.insert({name: "Brandeis University"});
+	Schools.insert({name: "Harvard University"});
+	Schools.insert({name: "Boston University"});
+	Schools.insert({name: "Massachusetts Institute of Technology"});
 
 
 })
