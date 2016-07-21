8cf95b41d023cb810439c53f587666e4bc4a4e4f
gclevin <Gabe@Gabriels-MacBook.local>
2016-07-21 14:52:30 -0400

    feedback added

--- pages/showAssignment/client/showAssignment.js
+++ pages/showAssignment/client/showAssignment.js
@@ -8,9 +8,31 @@ Template.showAssignment.helpers({
 
 
 Template.showSubmission.helpers({
+
+	feedback: function(){
+
+		console.log(this.metadata.assignment);
+		return Feedback.find({"id": this._id});
+	},
 	
 
 	assignments: function(){
 		return Assignments.find({"_id": this.metadata.assignment});
+	}
+})
+
+Template.showSubmission.events({
+
+	"click .js-feedback": function(event){
+		event.preventDefault();
+		console.log("worked");
+		const comment = $(".js-feedbackText").val();
+		const id = this._id;
+
+		const feedbackComment = {comment:comment, id:id}
+
+		console.log(feedbackComment);
+
+		Feedback.insert(feedbackComment);
 	}
 })
