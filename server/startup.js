834e9ef09226ac4214304b4aca017727ba23b6b1
gclevin <Gabe@Gabriels-MacBook.local>
2016-07-21 11:53:31 -0400

    Minor Changes

--- server/startup.js
+++ server/startup.js
@@ -1,15 +1,21 @@
 Meteor.startup(function(){
 	//Recordings.remove({});
-	Languages.insert({name: "English"});
-	Languages.insert({name: "German"});
-	Languages.insert({name: "Chinese"});
-	Languages.insert({name: "Spanish"});
-	Languages.insert({name: "French"});
 
-	Schools.insert({name: "Brandeis University"});
-	Schools.insert({name: "Harvard University"});
-	Schools.insert({name: "Boston University"});
-	Schools.insert({name: "Massachusetts Institute of Technology"});
+	if (Languages.find().count() == 0) {
+		Languages.insert({name: "English"});
+		Languages.insert({name: "German"});
+		Languages.insert({name: "Chinese"});
+		Languages.insert({name: "Spanish"});
+		Languages.insert({name: "French"});
+	}
+
+
+	if (Schools.find().count() == 0) {
+		Schools.insert({name: "Brandeis University"});
+		Schools.insert({name: "Harvard University"});
+		Schools.insert({name: "Boston University"});
+		Schools.insert({name: "Massachusetts Institute of Technology"});
+	}
 
 
 })
