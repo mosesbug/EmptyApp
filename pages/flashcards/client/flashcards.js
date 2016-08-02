5a4cd7418e350154668b8acc94be95bfa56556fb
gclevin <Gabe@Gabriels-MacBook.local>
2016-08-01 20:58:39 -0400

    Changed some appearance stuff

--- pages/flashcards/client/flashcards.js
+++ pages/flashcards/client/flashcards.js
@@ -106,39 +106,18 @@ Template.flashcardSet.events({
 
 	},
 
-	//  "click .js-submit": function(event){
-	//  	event.preventDefault();
-	//  	console.log("worked");
-	// 	const guess = $(".js-guess").val();
+	"click .js-delete": function(event){
+	 	event.preventDefault();
+		console.log("worked");
+		console.dir(this);
+		FlashcardPairs.remove(this.f._id);
 
-	// 	if (guess == x.wordTwo) {
-	// 		window.alert("Correct!");
-	// 		console.log("correct");
-	// 	} else {
-	// 		window.alert("Incorrect. Try again");
-	// 	}
-
-	// 	console.log(guess);
-
-
-	// },
+	},
 })
 
 
 Template.practiceFlashcards.events({
 
-	//  "click .js-addPair": function(event){
-	//  	event.preventDefault();
-	// 	const wordOne = $(".js-wordOne").val();
-	// 	const wordTwo = $(".js-wordTwo").val();
-	// 	const pair = {flashcardId:this._id, wordOne:wordOne, wordTwo:wordTwo}; 
-	// 	console.dir(pair);
-	// 	FlashcardPairs.insert(pair);
-
-	// 	$(".js-wordOne").val("");
-	// 	$(".js-wordTwo").val("");
-
-	// },
 
 	 "click .js-showAnswer": function(event, instance){
 	 	event.preventDefault();
@@ -207,4 +186,5 @@ Template.practiceFlashcards.events({
 
 
 	},
+
 })
